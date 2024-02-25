942ffcef07d19bf12f3eee2a8f13d8862ad7d353
tracer-ysyx <tracer@ysyx.org>
2024-02-26 00:05:19 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:05:19 up 3 days,  4:25,  1 user,  load average: 0.31, 0.25, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -19,13 +19,13 @@
 #include <readline/history.h>
 #include "sdb.h"
 
-static int is_batch_mode = false;
+static int is_batch_mode = false;//定义静态变量is_batch_mode用于指示调试器是否处于待处理模式，默认为false
 
-void init_regex();
-void init_wp_pool();
+void init_regex(); //初始化正则表达式
+void init_wp_pool(); //初始化观察点
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
-static char* rl_gets() {
+static char* rl_gets() { //定义函数rl_gets()获取命令行输入，支持历史记录功能
   static char *line_read = NULL;
 
   if (line_read) {
@@ -42,20 +42,34 @@ static char* rl_gets() {
   return line_read;
 }
 
-static int cmd_c(char *args) {
+static int cmd_c(char *args) { //cmd_c()函数用于使程序继续执行操作
   cpu_exec(-1);
   return 0;
 }
 
-
-static int cmd_q(char *args) {
+static int cmd_q(char *args) { //cmd_q()函数用于使程序停止操作
   nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
-static int cmd_help(char *args);
+static int cmd_si(char *args){ //cmd_si函数用于使程序单步执行
+  int n=0;
+  if(args==NULL){
+    n=1;
+  }
+  else {
+    sscanf(args,"%d",&n);
+  }
+  cpu_exec(n);
+  return 0;
+} 
 
-static struct {
+
+
+
+static int cmd_help(char *args); 
+
+static struct {//命令列表
   const char *name;
   const char *description;
   int (*handler) (char *);
@@ -63,14 +77,14 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si","Enter 'si [N]', let the program execute N instructions, and then pause",cmd_si}
   /* TODO: Add more commands */
 
 };
 
-#define NR_CMD ARRLEN(cmd_table)
+#define NR_CMD ARRLEN(cmd_table) //定义宏NR_CMD，记录命令表中命令的总数
 
-static int cmd_help(char *args) {
+static int cmd_help(char *args) {  //cmd_help()函数用于显示所有支持的命令及其描述
   /* extract the first argument */
   char *arg = strtok(NULL, " ");
   int i;
@@ -93,49 +107,55 @@ static int cmd_help(char *args) {
   return 0;
 }
 
-void sdb_set_batch_mode() {
+void sdb_set_batch_mode() { //设置待处理模式
   is_batch_mode = true;
 }
 
-void sdb_mainloop() {
-  if (is_batch_mode) {
+void sdb_mainloop() { //执行调试器的主循环逻辑
+
+  if (is_batch_mode) { //判断is_batch_mode,如果处于待处理模式，就开始执行CPU指令
     cmd_c(NULL);
     return;
   }
 
-  for (char *str; (str = rl_gets()) != NULL; ) {
-    char *str_end = str + strlen(str);
+//在个for循环用于从输入中逐行读取字符串，并对每行字符串进行解析和处理
+  for (char *str; (str = rl_gets()) != NULL; ) {//从输入中读取字符串赋值给str，只要读取的字符串不为NULL，就会继续执行循环
+    char *str_end = str + strlen(str);//每次循环计算字符串str的末尾位置，并将其赋值给字符指针变量str_end
 
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+//cmd指向命令
+    char *cmd = strtok(str, " ");//使用strtok函数从字符串str中，以空格为间隔，提取第一个子串并赋值给字符指针变量cmd
+    if (cmd == NULL) { continue; }//如果提取的标记为NULL，则执行continue跳过当前循环，继续下一次循环
 
-    /* treat the remaining string as the arguments,
-     * which may need further parsing
-     */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
+//args指向参数
+    char *args = cmd + strlen(cmd) + 1;//将指针 cmd 向后移动，跳过提取的标记，并指向剩余的字符串部分（即命令后面的参数部分）
+    if (args >= str_end) { //检查指针args是否超过了字符串str的末尾位置str_end。如果超过了末尾位置，则将 args 设置为NULL
       args = NULL;
     }
 
-#ifdef CONFIG_DEVICE
+#ifdef CONFIG_DEVICE //是否清空事件队列
     extern void sdl_clear_event_queue();
     sdl_clear_event_queue();
 #endif
 
+//这个for循环用于查找并执行 输入的命令行 所匹配的命令函数
     int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
-        break;
+    for (i = 0; i < NR_CMD; i ++) { //循环遍历，从索引0到NR_CMD - 1的范围内，对命令表进行查找和比较
+      if (strcmp(cmd, cmd_table[i].name) == 0) {//如果输入命令行的“命令部分”和定义的“命令名”相同，strcmp()函数返回0
+        if (cmd_table[i].handler(args) < 0) { 
+        //调用命令表中索引i处命令的处理函数handler，并传递 args参数。如果处理函数返回值小于0，使用return语句退出当前函数
+        //说明：如果小于 0，表示处理函数在执行过程中遇到了某种错误或异常情况，例如：1.错误码：处理函数定义了一些错误码，小于 0 的返回值表示出现了某种错误情况 2.异常情况：处理函数在执行过程中发生了异常，返回负值表示无法正常处理命令。
+          return; 
+        }
+        break;//跳出循环，因为已经找到并执行了匹配的命令处理函数
       }
     }
 
     if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    //如果循环结束时，遍历至i等于NR_CMD，表示未找到匹配的命令，打印“没有找到该命令“
   }
 }
 
-void init_sdb() {
+void init_sdb() { //初始化调试器相关配置
   /* Compile the regular expressions. */
   init_regex();
 
