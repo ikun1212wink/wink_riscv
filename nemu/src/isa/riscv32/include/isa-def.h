754f32630663e356ce961c7c5bd2a5c7e65eb1de
tracer-ysyx <tracer@ysyx.org>
2024-02-26 08:35:53 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:35:53 up 31 min,  1 user,  load average: 0.32, 0.21, 0.23

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -17,9 +17,10 @@
 #define __ISA_RISCV_H__
 
 #include <common.h>
-
+//定义了一个结构体类型riscv64_CPU_state或riscv32_CPU_state，具体根据宏CONFIG_RV64的定义来决定
+//这个结构体包含了 RISC-V 架构的通用寄存器（gpr 数组）和程序计数器（pc）
 typedef struct {
-  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
+  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];//如果 CONFIG_RVE 宏定义为真值（非零），则宏MUXDEF返回结果16，否则返回结果32
   vaddr_t pc;
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
