d97c27e0a15dd40f76dee08277a2c8eef03b6019
tracer-ysyx <tracer@ysyx.org>
2024-03-01 16:23:12 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:23:12 up 1 day,  6:11,  1 user,  load average: 0.13, 0.13, 0.12

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -278,7 +278,7 @@ int find(int p,int q){
     }
   }
   index--;
-  printf("%c%d%c%d%c%d%c%d",symbol_all[0].symbol,symbol_all[1].symbol,symbol_all[2].symbol,symbol_all[3].symbol
+  printf("%c%c%c%c%d%d%d%d",symbol_all[0].symbol,symbol_all[1].symbol,symbol_all[2].symbol,symbol_all[3].symbol
   ,symbol_all[0].pos,symbol_all[1].pos,symbol_all[2].pos,symbol_all[3].pos);
   for(int j=0;j<=index;j++){
     if(symbol_all[j].symbol=='+'||symbol_all[j].symbol=='-'){//将‘+’ ‘-’ 按顺序放入low_level
