66bc635bf5cf51e0b2d54b7b7aae654719e602c6
tracer-ysyx <tracer@ysyx.org>
2024-03-04 12:52:38 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:52:38 up 3 days, 16:29,  1 user,  load average: 0.59, 0.49, 0.45

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -93,7 +93,7 @@ static int cmd_x(char *args){
   args=args1+strlen(args1)+1;
   char *args2=strtok(args," ");
   
-  if(args1||args2==NULL){
+  if(args==NULL){
     printf("Unknow input, the standard format is \"x [N] EXPR\"\n");
   }
   else{
