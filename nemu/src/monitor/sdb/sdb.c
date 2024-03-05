6c4b4ddf393219443bb9aed3492707f221a74769
tracer-ysyx <tracer@ysyx.org>
2024-03-05 20:57:42 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:42 up 4 days, 15:02,  1 user,  load average: 1.56, 0.71, 0.43

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -105,7 +105,7 @@ static int cmd_x(char *args){
     sscanf(args2,"%x",&addr);
     //扫描内存
     for(int i=0;i<n;i++){
-      printf("0x%08x\n",paddr_read(addr,1));
+      printf("0x%08x\n",paddr_read(addr,4));
       addr+=4;
     }
   
