71faae1dd034c9ed46070c32d59519ec04f59500
tracer-ysyx <tracer@ysyx.org>
2024-03-07 17:48:59 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:48:59 up 55 min,  1 user,  load average: 0.41, 0.42, 0.31

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-#ifndef CONFIG_WATCHPOINT  
+#ifdef CONFIG_WATCHPOINT  
   printf("you define the CONFIG_WATCHPOINT\n");
 #endif  
   /* Start engine. */
