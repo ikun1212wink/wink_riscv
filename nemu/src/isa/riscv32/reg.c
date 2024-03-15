8705b11c844091e91d0e812943921a2184254dd0
tracer-ysyx <tracer@ysyx.org>
2024-03-15 19:44:45 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:45 up  3:37,  1 user,  load average: 0.38, 0.37, 0.26

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -22,8 +22,7 @@ const char *regs[] = {
     "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
     "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
     "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
-    "pc"};
+    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 void isa_reg_display()
 {
@@ -33,7 +32,7 @@ void isa_reg_display()
     printf("%s:\t 0x%08x\t %u\n", reg_name(i), cpu.gpr[i], cpu.gpr[i]);
   }
   //pc寄存器
-  printf("%s:\t 0x%08x\t %u\n",reg_name(32), cpu.pc, cpu.pc); //%08x用于以固定宽度（宽度为8）和零填充的形式打印无符号整数
+  printf("pc:\t 0x%08x\t %u\n", cpu.pc, cpu.pc); //%08x用于以固定宽度（宽度为8）和零填充的形式打印无符号整数
 }
 
 word_t isa_reg_str2val(const char *s, bool *success)
