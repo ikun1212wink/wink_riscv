e1c645828e3a54e5f4ed44479c8c78878d8fd4dd
tracer-ysyx <tracer@ysyx.org>
2024-04-05 22:36:43 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:43 up 2 days, 21:23,  1 user,  load average: 0.23, 0.24, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,72 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char HEX[] = "0123456789ABCDEF";
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char *out, const char *fmt, va_list ap){
+    char buffer[128];
+    char *txt;//字符串类型和字符类型
+    int num,len;
+    int state=0;
+    int i,j;
+    for(i=0,j=0;fmt[i]!='\0';i++){
+        switch(state){
+            case 0://正常复制
+                if(fmt[i]!='%'){
+                    out[j]=fmt[i];
+                    j++;
+                }
+                else{
+                    state=1;
+                }
+                break;
+            case 1://类型匹配
+                switch(fmt[i]){
+                    case 's':
+                        txt=va_arg(ap,char*);
+                        for(int k=0;txt[k]!='\0';k++){
+                            out[j]=txt[k];
+                            j++;
+                        }
+                        break;
+                    case 'd':
+                        num=va_arg(ap,int);
+                        if(num==0){
+                            out[j]='0';
+                            j++;
+                            break;
+                        }
+                        if(num<0){
+                            out[j]='-';
+                            j++;
+                            num=-num;
+                        }
+                        for(len=0;num;num/=10,len++){
+                            buffer[len]=HEX[num%10];
+                        }
+                        for(int k=len-1;k>=0;k--){
+                            out[j]=buffer[k];
+                            j++;
+                        }
+                        break;
+                }
+        }
+    }
+    out[j]='\0';
+    return j;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  int i;
+  va_start(args,fmt);
+  i=vsprintf(out,fmt,args);
+  va_end(args);
+  return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
