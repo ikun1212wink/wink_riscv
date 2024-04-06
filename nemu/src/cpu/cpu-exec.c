03aa9bb7b941f5d5297311dbd86c8d6b463ed031
tracer-ysyx <tracer@ysyx.org>
2024-04-07 01:30:31 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:30:31 up 3 days, 22:14,  1 user,  load average: 1.11, 0.71, 0.54

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -98,7 +98,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
-/*  printf("%s\n",s->logbuf);  */
+ printf("%s\n",s->logbuf); 
 #endif
 }
 
