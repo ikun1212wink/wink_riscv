724f63e3243401040ae43d9cb192cc569bf76dd6
tracer-ysyx <tracer@ysyx.org>
2024-04-07 14:20:39 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:20:39 up 4 days, 11:04,  1 user,  load average: 0.32, 0.32, 0.35

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -6,23 +6,21 @@ typedef struct {
   uint32_t inst;
 } ItraceNode;
 ItraceNode iringbuf[MAX_IRINGBUF];
-int pointer=0;
-bool full=false;
+int pointer=-1;
 void write_inst(word_t pc, uint32_t inst){
-    iringbuf[pointer].pc = pc;
-    iringbuf[pointer].inst = inst;
     if(pointer<MAX_IRINGBUF){
         pointer++;
     }
     else if(pointer==MAX_IRINGBUF){
         pointer=0;
-        full=true;
     }
+    iringbuf[pointer].pc = pc;
+    iringbuf[pointer].inst = inst;
 }
 
 void output_inst(){
     for(int i=0;i<MAX_IRINGBUF;i++){
-        if(i==(pointer-1)){
+        if(i==(pointer)){
             printf("--> 0x%x\t0x%x\n",iringbuf[i].pc,iringbuf[i].inst);
         }
         else{
