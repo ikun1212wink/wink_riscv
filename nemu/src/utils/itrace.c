015650d02cf2274b4c0195ad0866c874946a9ae5
tracer-ysyx <tracer@ysyx.org>
2024-04-07 14:22:42 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:42 up 4 days, 11:06,  1 user,  load average: 0.52, 0.39, 0.37

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -21,10 +21,10 @@ void write_inst(word_t pc, uint32_t inst){
 void output_inst(){
     for(int i=0;i<MAX_IRINGBUF;i++){
         if(i==(pointer)){
-            printf("--> 0x%x\t0x%x\n",iringbuf[i].pc,iringbuf[i].inst);
+            printf("--> 0x%x:\t%08x\n",iringbuf[i].pc,iringbuf[i].inst);
         }
         else{
-            printf("    0x%x\t0x%x\n",iringbuf[i].pc,iringbuf[i].inst);
+            printf("    0x%x:\t%08x\n",iringbuf[i].pc,iringbuf[i].inst);
         }
     }
 }
