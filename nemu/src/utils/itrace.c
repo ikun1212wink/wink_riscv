f4f3a818b8021b4b48d50eb18bfec88701ec5556
tracer-ysyx <tracer@ysyx.org>
2024-05-29 23:31:23 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:31:23 up 14:21,  1 user,  load average: 0.79, 0.81, 0.56

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -1,4 +1,5 @@
 #include <common.h>
+
 //iringbuf
 #define MAX_IRINGBUF 16
 
@@ -34,4 +35,13 @@ void output_inst(){
         }
     }
 }
+
+//mtrace
+void display_pread(paddr_t addr,int len){
+    printf("pread at "FMT_PADDR", len=%d\n",addr,len);
+}
+void display_pwrite(paddr_t addr,int len,word_t data){
+    printf("pwrite at "FMT_PADDR", len=%d, data="FMT_WORD"\n",addr,len,data);
+}
+
 
