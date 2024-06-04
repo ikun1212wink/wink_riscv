dee004557512bbd66729fdb33131d666445a2d7c
tracer-ysyx <tracer@ysyx.org>
2024-06-04 18:29:00 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:29:00 up  6:11,  1 user,  load average: 0.67, 0.56, 0.45

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -23,7 +23,7 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
 void init_sdb();
 void init_disasm(const char *triple);
-//void parse_elf(const char *elf_file);
+void parse_elf(const char *elf_file);
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
@@ -133,7 +133,7 @@ void init_monitor(int argc, char *argv[]) {
   init_sdb();
 
   /* Initialize elf */
-  //parse_elf(elf_file);
+  parse_elf(elf_file);
 
 #ifndef CONFIG_ISA_loongarch32r
   IFDEF(CONFIG_ITRACE, init_disasm(
--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include <elf.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 //iringbuf
 #define MAX_IRINGBUF 16
 
@@ -54,3 +55,75 @@ void trace_func_call(paddr_t pc, paddr_t target){
 void trace_func_ret(paddr_t pc){
     printf("return to 0x%x\n",pc);
 }
+
+void parse_elf(const char *elf_file) {
+    FILE *file = fopen(elf_file, "rb");
+    if (!file) {
+        perror("打开文件失败");
+        return;
+    }
+
+    // 读取 ELF 头
+    Elf32_Ehdr ehdr;
+    if (fread(&ehdr, 1, sizeof(ehdr), file) != sizeof(ehdr)) {
+        perror("读取 ELF 头失败");
+        fclose(file);
+        return;
+    }
+
+    // 定位到节头表的开始位置并读取
+    fseek(file, ehdr.e_shoff, SEEK_SET);
+    Elf32_Shdr *shdrs = malloc(ehdr.e_shnum * sizeof(Elf32_Shdr));
+    if (shdrs == NULL) {
+        perror("内存分配失败");
+        fclose(file);
+        return;
+    }
+    if (fread(shdrs, sizeof(Elf32_Shdr), ehdr.e_shnum, file) != ehdr.e_shnum) {
+        perror("读取节头表失败");
+        free(shdrs);
+        fclose(file);
+        return;
+    }
+
+    // 定位到字符串表：节头表中第 ehdr.e_shstrndx 条目指向字符串表
+    Elf32_Shdr strTabSec = shdrs[ehdr.e_shstrndx];
+    char *strTab = malloc(strTabSec.sh_size);
+    if (strTab == NULL) {
+        perror("内存分配失败");
+        free(shdrs);
+        fclose(file);
+        return;
+    }
+    fseek(file, strTabSec.sh_offset, SEEK_SET);
+    if (fread(strTab, 1, strTabSec.sh_size, file) != strTabSec.sh_size) {
+        perror("读取字符串表失败");
+        free(strTab);
+        free(shdrs);
+        fclose(file);
+        return;
+    }
+
+    // 遍历节头表，打印每个节的名称
+    for (int i = 0; i < ehdr.e_shnum; ++i) {
+        Elf32_Shdr shdr = shdrs[i];
+        if (shdr.sh_name < strTabSec.sh_size) {
+            printf("Section %d: %s\n", i, &strTab[shdr.sh_name]);
+        } else {
+            printf("Section %d: <节名超出字符串表范围>\n", i);
+        }
+    }
+
+    // 清理资源
+    free(strTab);
+    free(shdrs);
+    fclose(file);
+}
+
+
+
+
+
+
+
+
