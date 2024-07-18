c1071620e318113a4479f7d86ee3a174c5c77baf
2022280391-Zheng Weikai <2327431832@qq.com>
2024-07-18 16:05:16 +0800

    添加了common.h

---
+++ npc/include/common.h
@@ -0,0 +1,15 @@
+#ifndef __COMMON__H__
+#define __COMMON__H__
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vtop.h"
+#include <getopt.h>
+#define COLOR_RED     "\033[0;31m"
+#define COLOR_GREEN   "\033[0;32m"
+#define COLOR_YELLOW  "\033[0;33m"
+#define COLOR_BLUE    "\033[0;34m"
+#define COLOR_MAGENTA "\033[0;35m"
+#define COLOR_CYAN    "\033[0;36m"
+#define COLOR_RESET   "\033[0m"
+
+#endif  //!__COMMON__H__
--- npc/obj_dir/Vtop.mk
+++ npc/obj_dir/Vtop.mk
@@ -35,6 +35,7 @@ VM_PREFIX = Vtop
 VM_MODPREFIX = Vtop
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
+	-I/home/wink/ysyx-workbench/npc/include \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
