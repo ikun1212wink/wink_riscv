3fd2c95443706eea918aa8fa7fcf223717c7330e
tracer-ysyx <tracer@ysyx.org>
2024-07-18 11:01:53 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:01:53 up 3 days,  1:55,  2 users,  load average: 0.46, 0.50, 0.41

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,14 +1,5 @@
 #include <common.h>
-
-
-#define COLOR_RED     "\033[0;31m"
-#define COLOR_GREEN   "\033[0;32m"
-#define COLOR_YELLOW  "\033[0;33m"
-#define COLOR_BLUE    "\033[0;34m"
-#define COLOR_MAGENTA "\033[0;35m"
-#define COLOR_CYAN    "\033[0;36m"
-#define COLOR_RESET   "\033[0m"
-
+#include <utils.h>
 
 int ebreak_flag=0;
 int mem_number;
