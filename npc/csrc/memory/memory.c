9869bc9b7f2af5670866b3ab4d428cf4228b81e6
tracer-ysyx <tracer@ysyx.org>
2024-07-18 19:39:09 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:39:09 up 3 days, 10:32,  2 users,  load average: 0.65, 0.40, 0.28

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -1,6 +1,7 @@
 #include <memory.h>
 #include <monitor.h>
 extern char *img_path;
+int mem_number;
 uint32_t guest_to_host(uint32_t addr){ //虚拟地址转换成物理地址
   return addr-0x80000000;
 }
@@ -10,7 +11,8 @@ uint32_t pmem_read(uint32_t*memory,uint32_t vaddr){ //物理地址读取函数
   return memory[paddr/4];
 }
 
-uint32_t* init_mem(int* num) { //初始化内存
+uint32_t* init_mem() { //初始化内存
+    int* num=&mem_number;
     FILE* file = fopen(img_path, "rb");
     if (!file) {
         printf("Failed to open file: %s\n", img_path);
