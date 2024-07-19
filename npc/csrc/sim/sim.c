8d0e204e21435daa323f628c7db3a86dc8b9a3a8
tracer-ysyx <tracer@ysyx.org>
2024-07-19 11:20:46 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:20:46 up 14:45,  1 user,  load average: 0.62, 0.50, 0.47

--- npc/csrc/sim/sim.c
+++ npc/csrc/sim/sim.c
@@ -31,6 +31,7 @@ void reset(int n){ //复位函数
 }
 
 void reg_p(){
+  single_cycle();
   Vtop___024root* root = dut.rootp;
   for(int i=0; i < 32; i++){
     printf("reg[%d]:0x%08x\n",i,root->top__DOT__Register__DOT__rf[i]);
