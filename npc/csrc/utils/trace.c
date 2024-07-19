7b78dbbd624b86cb298d6c40d3dba5b1844bb71b
tracer-ysyx <tracer@ysyx.org>
2024-07-19 21:10:26 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:10:26 up  1:39,  1 user,  load average: 0.54, 0.53, 0.46

--- npc/csrc/utils/trace.c
+++ npc/csrc/utils/trace.c
@@ -4,7 +4,7 @@
 #include <locale.h>
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 void itrace(){
-/*     char logbuf[1000000000000000];
+    char logbuf[128];
     char *p=logbuf;
     p += snprintf(p, sizeof(logbuf), FMT_WORD ":", dut.pc);
     int ilen=4;
@@ -19,17 +19,11 @@ void itrace(){
     space_len = space_len * 3 + 1;
     memset(p, ' ', space_len);
     p += space_len;
-    disassemble(p, logbuf + sizeof(logbuf) - p,
-    dut.pc, (uint8_t *)&dut.inst, ilen);  */
+    disassemble(p, sizeof(logbuf),
+    dut.pc, (uint8_t *)&dut.inst, ilen); 
 
-    char result[256];
 
-    // 定义指令地址和指令数据
-    uint64_t pc = 0x12345678;
-    uint8_t code[] = {0x90, 0x90, 0x90, 0x90}; // 示例数据，你需要提供实际的指令数据和长度
 
-    // 调用 disassemble 函数获取反汇编结果
-    disassemble(result, 100000000, pc, code, sizeof(code));
 
-    printf("%s\n",result);
+    printf("%s\n",logbuf);
 }
