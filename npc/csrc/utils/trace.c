2d94fe81f6ffc75c880c158dcdc9bf4a086b00f0
tracer-ysyx <tracer@ysyx.org>
2024-07-19 21:14:55 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:55 up  1:43,  1 user,  load average: 0.33, 0.41, 0.43

--- npc/csrc/utils/trace.c
+++ npc/csrc/utils/trace.c
@@ -19,11 +19,7 @@ void itrace(){
     space_len = space_len * 3 + 1;
     memset(p, ' ', space_len);
     p += space_len;
-    disassemble(p, sizeof(logbuf),
+    disassemble(p, sizeof(logbuf) - (p - logbuf),
     dut.pc, (uint8_t *)&dut.inst, ilen); 
-
-
-
-
     printf("%s\n",logbuf);
 }
