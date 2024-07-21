903c702598e148cb20afaaa491ddb5db6ae4d0a3
tracer-ysyx <tracer@ysyx.org>
2024-07-21 11:51:41 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:51:41 up 1 day, 16:20,  1 user,  load average: 0.51, 0.45, 0.45

--- npc/csrc/cpu/cpu-exe.c
+++ npc/csrc/cpu/cpu-exe.c
@@ -11,12 +11,13 @@ int ebreak_flag=0;
 
  void execute(int n){
     uint32_t*memory=init_mem();
+    dut.pc=0x80000000;
     if(n>0){
         for (;n > 0; n --) {     
             printf(COLOR_BLUE "pc:  0x%x" COLOR_RESET "\n",dut.pc);
             dut.inst=pmem_read(memory,dut.pc);//这个函数是每个周期才会执行一次（下降沿），而PC增加是在上升沿，所以就错开了半个周期
             printf(COLOR_CYAN "inst:0x%08x" COLOR_RESET "\n",dut.inst);
-           // difftest_step(dut.pc,dut.pc);
+            difftest_step(dut.pc,dut.pc);
             itrace();
             single_cycle();
         }
