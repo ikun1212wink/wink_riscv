2e8b32bdde7a1052faee12450ade89ced406c22a
tracer-ysyx <tracer@ysyx.org>
2024-07-21 12:05:28 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:05:28 up 1 day, 16:34,  1 user,  load average: 0.35, 0.38, 0.40

--- npc/csrc/sim/sim.c
+++ npc/csrc/sim/sim.c
@@ -8,6 +8,7 @@ void sim_init(){ //波形仿真使能函数
   Verilated::traceEverOn(true);
   contextp=new VerilatedContext;
   tfp=new VerilatedVcdC;
+  dut.pc=0x80000000;
   dut.trace(tfp,0);
   tfp->open("wave.vcd");
 }
