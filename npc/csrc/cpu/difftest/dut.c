9ceb3ebe11b9235c901a4233a8c3490b47028d5a
tracer-ysyx <tracer@ysyx.org>
2024-07-21 11:40:20 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:40:20 up 1 day, 16:08,  1 user,  load average: 0.76, 0.50, 0.48

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -78,7 +78,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   memcpy(&npc_dut.pc,&dut.pc,sizeof(dut.pc));
   memcpy(npc_dut.gpr,&(dut.rootp->top__DOT__Register__DOT__rf),sizeof(dut.rootp->top__DOT__Register__DOT__rf));
 
-  uint32_t reset_vector=(uint32_t)0xFFFFFFFF;
+  uint32_t reset_vector=(uint32_t)0x80000000;
   uint32_t *npc_guest_to_host=init_mem();//npc程序实际位置的指针
 
   ref_difftest_init(port);
