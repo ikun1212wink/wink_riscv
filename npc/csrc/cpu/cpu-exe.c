63186035c9e53dcdbb8434b5fdfd7f3d6a6d3064
tracer-ysyx <tracer@ysyx.org>
2024-07-22 20:50:47 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:47 up 10:35,  1 user,  load average: 0.57, 0.35, 0.30

--- npc/csrc/cpu/cpu-exe.c
+++ npc/csrc/cpu/cpu-exe.c
@@ -7,10 +7,12 @@ extern VerilatedContext* contextp ;
 extern VerilatedVcdC* tfp ;
 extern Vtop dut;
 int ebreak_flag=0;
-
+int exe=0;
  void execute(int n){
     if(n>0){
-        for (;n > 0; n --) {     
+        for (;n > 0; n --) {   
+            exe++;
+            printf("执行指令数%d\n",exe);  
             printf(COLOR_BLUE "pc:  0x%x" COLOR_RESET "\n",dut.pc);
             //dut.inst=pmem_read(memory,dut.pc);//这个函数是每个周期才会执行一次（下降沿），而PC增加是在上升沿，所以就错开了半个周期
             printf(COLOR_CYAN "inst:0x%08x" COLOR_RESET "\n",dut.inst);
@@ -26,6 +28,8 @@ int ebreak_flag=0;
     }
     else if(n<0) {
         while(!ebreak_flag){  
+            exe++;
+            printf("执行指令数%d\n",exe);  
             printf(COLOR_BLUE "pc:  0x%x" COLOR_RESET "\n",dut.pc);
             //dut.inst=pmem_read(memory,dut.pc);
             printf(COLOR_CYAN "inst:0x%08x" COLOR_RESET "\n",dut.inst);
