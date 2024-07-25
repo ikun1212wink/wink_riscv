74b15d1e354afee8b0430578534332930d6fcbf3
tracer-ysyx <tracer@ysyx.org>
2024-07-25 15:01:35 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:01:35 up  5:08,  1 user,  load average: 0.21, 0.24, 0.27

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,12 +29,24 @@ int atoi(const char* nptr) {
   return x;
 }
 
+char* currentAddr = NULL;
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if(currentAddr == NULL){
+    currentAddr =(void *)ROUNDUP(heap.start, 8);
+  }
+  size = (size_t)ROUNDUP(size, 8);
+  char* old = currentAddr;
+  currentAddr += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)currentAddr && (uintptr_t)currentAddr < (uintptr_t)heap.end);
+  for(uint64_t *p = (uint64_t*) old; p!= (uint64_t *)currentAddr; p ++){
+    *p = 0;
+  }
+  return old;
 #endif
   return NULL;
 }
