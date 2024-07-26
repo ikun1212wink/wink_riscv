a598609b3483955d55ccfad690f0928cdef3b6b5
tracer-ysyx <tracer@ysyx.org>
2024-07-26 10:19:42 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:19:42 up 1 day, 26 min,  1 user,  load average: 0.31, 0.39, 0.53

--- nemu/include/trace.h
+++ nemu/include/trace.h
@@ -6,6 +6,5 @@ void trace_dread(paddr_t addr, int len, IOMap *map);
 void trace_dwrite(paddr_t addr, int len, word_t data, IOMap *map);
 void output_inst();
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-#define DTRACE
-/* #define ITRACE */
+
 #endif  //!__TRACE__H__
