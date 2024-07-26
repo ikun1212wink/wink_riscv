7d52c447a64956e4d4b9a31b2a62936f6fc93bc4
tracer-ysyx <tracer@ysyx.org>
2024-07-26 15:46:15 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:15 up 1 day,  5:53,  1 user,  load average: 0.37, 0.45, 0.45

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,23 +3,15 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 #define N   32
-uint32_t h;
-uint32_t w;
+
 void __am_gpu_init() {
-  int i;
-/*   int w = 0;  // TODO: get the correct width
-  int h = 0;  // TODO: get the correct height */
-  int w_init =  w/ N;
-  int h_init =  h/ N;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w_init * h_init; i ++) fb[i] = i;
-    outl(SYNC_ADDR, 1);
+
 }
 //AM显示控制器信息, 可读出屏幕大小信息width和height. 另外AM假设系统在运行过程中, 屏幕大小不会发生变化.
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t screen_wh=inl(VGACTL_ADDR);
-  h=screen_wh&0x0000FFFF;
-  w=screen_wh>>16;
+  uint32_t h=screen_wh&0x0000FFFF;
+  uint32_t w=screen_wh>>16;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = w, .height = h,
@@ -28,7 +20,22 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 //AM帧缓冲控制器, 可写入绘图信息, 向屏幕(x, y)坐标处绘制w*h的矩形图像. 图像像素按行优先方式存储在pixels中, 
 //每个像素用32位整数以00RRGGBB的方式描述颜色. 若sync为true, 则马上将帧缓冲中的内容同步到屏幕上
+/* void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
+} */
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
