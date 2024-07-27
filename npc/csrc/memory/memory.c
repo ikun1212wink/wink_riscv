7790759023fda1d8e0ac23882689e8e9b25d73af
tracer-ysyx <tracer@ysyx.org>
2024-07-27 14:46:24 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:46:24 up 2 days,  4:53,  1 user,  load average: 0.65, 0.69, 0.62

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -73,7 +73,7 @@ long img_size(){
 
 
 extern "C" uint32_t pmem_read(int raddr) {
-  if(raddr>=0x80000092){
+  if(raddr>=0x80000091){
     difftest_skip_ref();
   }
   //uint32_t* memory=init_mem();
