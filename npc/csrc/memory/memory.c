21c55c265f48aee6b5dc81997b0d0a3a7ec7a199
tracer-ysyx <tracer@ysyx.org>
2024-07-27 11:54:04 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:54:04 up 2 days,  2:01,  1 user,  load average: 0.44, 0.35, 0.35

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -118,8 +118,8 @@ extern "C" void pmem_write(int waddr, int wdata, char select) {
   }
   if(waddr==0xa00003f8){
     memory[img_wr_addr/4]=new_mem_word;
-    char *data=(char*)memory[img_wr_addr/4];
-  //  putchar(*data);
+    char data = (char)(memory[img_wr_addr / 4] & 0xFF);  // 获取低字节的数据
+    putchar(data);
   }
   else{
     memory[img_wr_addr/4]=new_mem_word;
