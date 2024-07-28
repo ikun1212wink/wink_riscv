d3215c03e039642a75a59a66b392df995d2d8ca0
tracer-ysyx <tracer@ysyx.org>
2024-07-28 18:14:45 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:14:45 up 3 days,  8:21,  1 user,  load average: 0.58, 0.62, 0.55

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    printf("%x",c->mcause);
+    printf("hellohi%x\n",c->mcause);
     switch (c->mcause) {
       case 11:ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
