187fbc6fa28da6870eacfc793d5af8899cc92484
tracer-ysyx <tracer@ysyx.org>
2024-07-28 16:27:49 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:49 up 3 days,  6:34,  1 user,  load average: 0.27, 0.45, 0.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -138,6 +138,7 @@ void assert_fail_msg() {
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
+  cpu.mstatus=0x1800;
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
