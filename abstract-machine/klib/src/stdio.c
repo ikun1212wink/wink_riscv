53e7c8bff916655457629f9a6c8495fdaaeda4ae
tracer-ysyx <tracer@ysyx.org>
2024-07-30 16:22:26 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:22:26 up 22:38,  1 user,  load average: 0.23, 0.24, 0.17

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -122,6 +122,9 @@ int vsprintf(char *out, const char *fmt, va_list ap){
                                 }
                                 break;
                         default:
+                         putch(fmt[i-1]);
+                        putch(fmt[i]);
+                         putch(fmt[i+1]);
                         assert(0);
                     }
                 }
