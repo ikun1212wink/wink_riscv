db8dfd4319313e22f93a42330d90acddc3eb4df4
tracer-ysyx <tracer@ysyx.org>
2024-07-30 21:36:19 +0800

    > compile NEMU ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:19 up 1 day,  3:51,  1 user,  load average: 0.44, 0.21, 0.11

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -43,7 +43,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c=(Context *)kstack.end-1;
   c->mepc=(uintptr_t)entry;
   c->mstatus=0x1800;
-  c->mcause=0xb;
+  //c->mcause=0xb;
   c->gpr[10]=(uintptr_t)arg;//a0寄存器用来传递函数参数，以及保存函数的返回值
   return c;
 }
