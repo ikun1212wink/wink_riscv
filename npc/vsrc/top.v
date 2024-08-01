e0835b80fedb258385ab7e50d8f0034d6d420be4
tracer-ysyx <tracer@ysyx.org>
2024-08-01 17:00:53 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:53 up 2 days, 23:16,  1 user,  load average: 0.29, 0.23, 0.21

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -47,7 +47,7 @@ wire [31:0] imm_out;
 assign alu_a=alu_a_sel ? rs1_data : pc ;
 assign alu_b=(alu_b_sel==2'b00) ? rs2_data :
              (alu_b_sel==2'b01) ? 0 :
-             (alu_b_sel==2'b10) ? 32'h0 :
+             (alu_b_sel==2'b10) ? r_csr_data :
              (alu_b_sel==2'b11) ? imm_out : 32'h0;
 
 assign pc_plus4=pc+32'h4;
