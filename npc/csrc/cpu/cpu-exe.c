cd6d3b82feebde5330dc27ccfceb9457d9c1ce01
tracer-ysyx <tracer@ysyx.org>
2024-08-02 11:15:47 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:47 up 15:26,  1 user,  load average: 0.42, 0.40, 0.32

--- npc/csrc/cpu/cpu-exe.c
+++ npc/csrc/cpu/cpu-exe.c
@@ -34,11 +34,11 @@ void itrace_printf(){
             #ifdef  REG_P
                 reg_p();
             #endif  //REG_P    
-             dump_wave();
-        dut.final();
-        tfp->close();                   
+                               
         }
-       
+        dump_wave();
+        dut.final();
+        tfp->close();
     }
     else if(n<0) {
         while(!ebreak_flag){  
