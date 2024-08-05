2d7b619cc1586002efec1da11167fd33b01d10bb
tracer-ysyx <tracer@ysyx.org>
2024-08-05 12:21:04 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:21:04 up 1 day, 18:21,  1 user,  load average: 0.34, 0.41, 0.44

--- npc/vsrc/LSU.v
+++ npc/vsrc/LSU.v
@@ -61,7 +61,7 @@ ysyx_23060240_SRAM_LSU SRAM_LSU(
     .waddr(mem_wr_addr),
     .wmask(memory_wr_ctrl),
     .w_en(mem_wr_en),
-    .r_en(0),
+    .r_en(1),
     .wdata(mem_wr_data),
     .rdata(mem_out)
 );
