4e8e24d771818d1592434982b5be506dd402ac2c
2022280391-Zheng Weikai <2327431832@qq.com>
2024-08-06 12:03:04 +0800

    NPC的AXI实现中

--- npc/vsrc/SRAM_IFU.v
+++ npc/vsrc/SRAM_IFU.v
@@ -1,15 +1,32 @@
 module ysyx_23060240_SRAM_IFU(
      input clk,
-     input [31:0] raddr,
-     input r_en,
-     output reg [31:0] rdata
+     input rst,
+    //AXI_LITE BUS
+    //write address channel
+/*     input [4:0] saxi_awaddr,
+    input saxi_awvalid,
+    output saxi_awready, */
+    //read address channel
+    input [31:0] saxi_araddr,
+    input saxi_arvalid,   
+    output saxi_arready,
+    //write data channel
+/*     input [31:0] saxi_wdata,
+    input saxi_wvalid,
+    output saxi_wready, */
+    //read data channel
+    input saxi_rready,
+    output saxi_rvalid,
+    output [31:0] saxi_rdata
+    //write response channel
+/*     input saxi_bready,
+    output saxi_bvalid */
 );
 
-reg [31:0] rdata_temp;
-
-import "DPI-C" function int pmem_read(input int raddr);
+/* reg [31:0] rdata_temp;
+import "DPI-C" function int pmem_read(input int raddr); */
 /* verilator lint_off LATCH */
-always@(*)begin
+/* always@(*)begin
      if(r_en)begin
           rdata_temp=pmem_read(raddr);
      end
@@ -19,5 +36,5 @@ always@(*)begin
 end
 always@(posedge clk)begin
      rdata<=rdata_temp;
-end  
+end  */ 
 endmodule
