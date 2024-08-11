7beddfc3a95c5d3b171cc246fe412ec28d9b3a01
tracer-ysyx <tracer@ysyx.org>
2024-08-11 22:10:17 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:10:17 up 8 days,  4:10,  1 user,  load average: 0.71, 0.56, 0.39

--- npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0.cpp
+++ npc/obj_dir/Vtop___024root__DepSet_heccd7ead__0.cpp
@@ -440,12 +440,10 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
                     & (IData)(vlSelf->top__DOT__lsu_arvalid))) {
             if (((0xa0000048U == vlSelf->top__DOT__alu_out) 
                  | (0xa000005cU == vlSelf->top__DOT__alu_out))) {
-                __Vdly__top__DOT__ARB__DOT__wait_read = 1U;
                 __Vdly__top__DOT__ARB__DOT__arb_ready = 0U;
                 __Vdly__top__DOT__ARB__DOT__state = 7U;
             } else {
                 __Vdly__top__DOT__ARB__DOT__arb_ready = 0U;
-                __Vdly__top__DOT__ARB__DOT__wait_read = 1U;
                 __Vdly__top__DOT__ARB__DOT__state = 2U;
             }
         } else if (((IData)(vlSelf->top__DOT__ARB__DOT__arb_ready) 
