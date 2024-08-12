5b32612e36743117c1715e287d4d01ca9c6e3de2
tracer-ysyx <tracer@ysyx.org>
2024-08-12 16:58:57 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:57 up 8 days, 22:59,  1 user,  load average: 0.20, 0.27, 0.23

--- npc/csrc/cpu/cpu-exe.c
+++ npc/csrc/cpu/cpu-exe.c
@@ -40,6 +40,8 @@ void itrace_printf(){
     else if(n<0) {
         while(!ebreak_flag){  
             exe++;
+
+            single_cycle();
             if(dut.itrace_reg){
                 #ifdef  ITRACE
                     itrace_printf();
@@ -48,7 +50,6 @@ void itrace_printf(){
                     reg_p();
                 #endif  //REG_P 
             }  
-            single_cycle();
             if(dut.difftest){
                 #ifdef  ITRACE
                     itrace_printf();
