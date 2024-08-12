ba2f683df9126917d378cba35ea0c6e9f6b9fee3
tracer-ysyx <tracer@ysyx.org>
2024-08-12 09:37:40 +0800

    > sim RTL ysyx_2022280391 ZhengWeikai Linux wink-virtual-machine 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:37:40 up 8 days, 15:37,  1 user,  load average: 0.39, 0.11, 0.04

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -9,8 +9,8 @@ uint32_t boot_time_lo;
 uint64_t boot_time = 0;
 
 void __am_timer_init() {
-  boot_time_hi = inl(RTC_ADDR+4);
-  boot_time_lo = inl(RTC_ADDR);
+  boot_time_hi = inl(RTC_ADDR+4)/2;
+  boot_time_lo = inl(RTC_ADDR)/2;
   boot_time=(uint64_t)boot_time_hi<<32|boot_time_lo;
 }
 
